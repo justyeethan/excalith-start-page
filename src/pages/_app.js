25a74b6034745a7039671b93fc9a3c6a1f013f85
Ethan <diamondspectreme@gmail.com>
2024-03-23 17:52:08 -0700

    Fix analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import React from "react"
 import { ErrorBoundary } from "react-error-boundary"
 import "@/styles/globals.css"
 import { SettingsProvider } from "@/context/settings"
+import { Analytics } from "@vercel/analytics/react"
 
 function fallbackRender({ error, resetErrorBoundary }) {
 	const isDataError = error.message.includes("undefined")
@@ -74,9 +75,11 @@ function fallbackRender({ error, resetErrorBoundary }) {
 export default function App({ Component, pageProps }) {
 	return (
 		<ErrorBoundary fallbackRender={fallbackRender}>
-			<SettingsProvider>
-				<Component {...pageProps} />
-			</SettingsProvider>
+			<Analytics>
+				<SettingsProvider>
+					<Component {...pageProps} />
+				</SettingsProvider>
+			</Analytics>
 		</ErrorBoundary>
 	)
 }
